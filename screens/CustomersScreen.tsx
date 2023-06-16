7e673dc9d5fab08ae5f9661a7d56ca1ac0e0d336
Aswin Karki <Aswin.karki44@gmail.com>
2023-06-16 21:18:01 +0545

    configure stepzen for graphql backend

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,14 +1,52 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
-import SafeAreaViewAndroid from "../components/SafeAreaViewAndroid";
+import { View, Text, ScrollView, ActivityIndicator } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { TabStackParamList } from "../navigator/TabNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { Image } from "@rneui/themed";
+import { Input } from "@rneui/base";
+
+export type CutomerScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Customers">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 type Props = {};
 
 const CustomersScreen = (props: Props) => {
+  const navigation = useNavigation<CutomerScreenNavigationProp>();
+  const [input, setInput] = useState<string>("");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <SafeAreaView style={SafeAreaViewAndroid.AndroidSafeArea}>
-      <Text>CustomersScreen</Text>
-    </SafeAreaView>
+    <ScrollView style={{ backgroundColor: "#59C1CC" }}>
+      <Image
+        source={{ uri: "https://links.papareact.com/3jc" }}
+        containerStyle={{ width: "100%", height: 256 }}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+      <Input
+        placeholder="Search By Customer"
+        value={input}
+        onChangeText={(text) => setInput(text)}
+        containerStyle={{
+          backgroundColor: "white",
+          paddingTop: 20,
+          paddingBottom: 0,
+          paddingHorizontal: 40,
+        }}
+      />
+    </ScrollView>
   );
 };
 
